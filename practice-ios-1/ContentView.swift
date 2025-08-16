362c3ea7b8aeff75fb37d1f1ab279b87d3981e25
Eizaburo Tamaki <eizaburo.tamaki@gmail.com>
2025-08-17 08:00:50 +0900

    Initial Commit

---
+++ practice-ios-1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  practice-ios-1
+//
+//  Created by Eizaburo Tamaki on 2025/08/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ practice-ios-1/practice_ios_1App.swift
@@ -0,0 +1,17 @@
+//
+//  practice_ios_1App.swift
+//  practice-ios-1
+//
+//  Created by Eizaburo Tamaki on 2025/08/17.
+//
+
+import SwiftUI
+
+@main
+struct practice_ios_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
